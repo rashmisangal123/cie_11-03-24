4db37764879f0ae3d346aac61d9312abdc84dee4
rashmisangal123 <01fe22bca004@kletech.ac.in>
2024-03-11 16:06:07 +0530

    display the students course

---
+++ Students_course_information.java
@@ -0,0 +1,9 @@
+
+public class Students_course_information {
+    public String course1 ="c programming";
+    public String course2 ="devops";
+    public String marks1 ="c programming";
+    public String marks2 ="c programming";
+}
+
+
---
+++ student.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+class student extends Students_course_information{
+    static String name = "rashmi";
+    static int age = 19;
+    static String course1;
+    static String course2;
+    static String marks1;
+    static String marks2;
+
+public static void main(String[]args){
+
+    System.out.println("name = "+name);
+    System.out.println("age ="+age);
+    Date D1 = new Date();
+    System.out.println("Date =" +D1);
+    
+    System.out.println("course1 = "+"c progarming");
+   
+    System.out.println("course2 ="+"devops");
+    
+    System.out.println("marks1= "+80);
+  
+    System.out.println("marks2 = "+90);
+}}
+
+
+   
+
+
